64cffba5955c0778286b641f75c3d23c1d6d6898
j_buet09 <j_buet09@uni-muenster.de>
2018-11-28 15:14:25 +0100

    edit favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <link rel="shortcut icon" type="image/png" href="/img/FoxdoxChat.png"/>
+        <link rel="shortcut icon" type="image/png" href="/img/foxChat_kurz.png"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" type="text/css" href="/css/app.css">
