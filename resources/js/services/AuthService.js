77283f47cc0124b3c7f468981961bd4b813bc816
m4l3x <manuel.barkau@hotmail.de>
2018-11-29 17:12:49 +0100

    reverted changes

--- resources/js/services/AuthService.js
+++ resources/js/services/AuthService.js
@@ -1,3 +1,5 @@
+import cookies from '../cookies.js';
+
 const config = {
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
@@ -15,7 +17,7 @@ class AuthService {
         axios.post('/api/auth/login', formData, config)
             .then(response => {
                 console.log('Logging In...');
-                self.$cookies.set('bearer', response.data.access_token);
+                cookies.set('bearer', response.data.access_token);
 
                 this.retrieveUser(self);
                 
@@ -36,7 +38,7 @@ class AuthService {
 
     logout(self){
         var formData = new FormData();
-        formData.append('token', self.$cookies.get('bearer'));
+        formData.append('token', cookies.get('bearer'));
 
         axios.post('/api/auth/logout', formData, config)
             .then(response => {
@@ -46,8 +48,8 @@ class AuthService {
                 console.log('Error logging Out.');
             });
             
-        self.$cookies.remove('bearer');
-        self.$cookies.remove('user');
+        cookies.remove('bearer');
+        cookies.remove('user');
 
         self.$router.push('/login');
     }
@@ -58,16 +60,16 @@ class AuthService {
         var configExt = {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Bearer '+self.$cookies.get('bearer')
+                'Authorization': 'Bearer '+cookies.get('bearer')
             }
         }
         axios.get('/api/auth/me', configExt)
             .then(response => {
 
-                self.$cookies.set('user', JSON.stringify(response.data));
+                cookies.set('user', JSON.stringify(response.data));
 
                 console.log('Got Userdata:');
-                console.log(JSON.stringify(self.$cookies.get('user')));
+                console.log(JSON.stringify(cookies.get('user')));
             })
             .catch(error => {
                 this.logout();
