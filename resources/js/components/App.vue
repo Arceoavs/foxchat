91d8c48f7a0eeba42d588e9d7c5c8839590bbf61
a_arzv01 <a_arzv01@uni-muenster.de>
2018-12-04 00:58:16 +0100

    navbar mobile ready

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,22 +2,25 @@
   <div>
     <loading-component></loading-component>
     <b-navbar toggleable="md" class="navbar-laravel">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand to="/">
         <img class="img-logo" src="/img/FoxdoxChat.png">
       </b-navbar-brand>
-      <b-navbar-nav v-show="loggedIn">
-        <b-nav-item to="/index">
-          <div>Dokumente</div>
-        </b-nav-item>
-        <b-nav-item to="/Chat">
-          <div>Chat</div>
-        </b-nav-item>
-      </b-navbar-nav>
-      <b-navbar-nav class="ml-auto foxcolor max-height-nav" v-show="loggedIn">
-        <b-nav-item right class="logout-link">
-          <logout-component></logout-component>
-        </b-nav-item>
-      </b-navbar-nav>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav v-show="loggedIn">
+          <b-nav-item to="/index">
+            <div>Dokumente</div>
+          </b-nav-item>
+          <b-nav-item to="/Chat">
+            <div>Chat</div>
+          </b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto foxcolor max-height-nav" v-show="loggedIn">
+          <b-nav-item right class="logout-link">
+            <logout-component></logout-component>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
     <!--<div class="clearfisx"></div>-->
     <router-view></router-view>
