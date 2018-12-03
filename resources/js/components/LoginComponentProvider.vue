f7c9789dda97d5b08aeea564fd47e630e2396ef9
a_arzv01 <a_arzv01@uni-muenster.de>
2018-12-03 23:12:28 +0100

    Provider-login mobile ready

--- resources/js/components/LoginComponentProvider.vue
+++ resources/js/components/LoginComponentProvider.vue
@@ -1,56 +1,41 @@
 <template>
-  <!--<div class="container myCont">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card card-default">
-                    <div class="card-header">Login</div>
-                    <div @keyup.enter="sendPassword()" class="card-body  mx-auto">
-                        <input v-model="username" placeholder="Username">
-                        <input type="password" v-model="password" placeholder="Password">
-                        <input v-model="xProvider" placeholder="X-Provider ID">
-                        <button type="button" class="btn foxcolor" v-on:click="sendPassword()" >Login</button>
-
-                        <b-alert :show="showAlert" @dismissed="showAlert = false" variant="danger" dismissible>{{ errorMsg }}</b-alert>
-
-                        <p><router-link to="/login/">
-                            <a class="">No Provider? Get to our User Form</a>
-                        </router-link></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-  </div>-->
   <div class="login-form">
-    <div class="main-div">
-      <div class="panel">
-        <h2>Anmeldung</h2>
-        <p>Melden sie sich mit ihrem foxdox Konto an:</p>
+    <div class="row justify-content-center">
+      <div class="col-4 col-md-4 col-sm-8">
+        <div class="panel">
+          <h2>Anmeldung</h2>
+          <p>Melden sie sich mit ihrem foxdox Konto an:</p>
+        </div>
       </div>
-      <div @keyup.enter="sendPassword()">
-        <div class="form-group">
-          <input v-model="username" class="form-control" placeholder="Benutzername">
-        </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-8 col-md-4 col-sm-6">
+        <div @keyup.enter="sendPassword()">
+          <div class="form-group">
+            <input v-model="username" class="form-control" placeholder="Benutzername">
+          </div>
 
-        <div class="form-group">
-          <input v-model="password" type="password" class="form-control" placeholder="Passwort">
-        </div>
+          <div class="form-group">
+            <input v-model="password" type="password" class="form-control" placeholder="Passwort">
+          </div>
 
-        <div class="form-group">
-          <input v-model="xProvider" class="form-control" placeholder="X-Provider ID">
-        </div>
-        <div class="description">
-          <b-alert
-            :show="showAlert"
-            @dismissed="showAlert = false"
-            variant="danger"
-            dismissible
-          >{{ errorMsg }}</b-alert>
+          <div class="form-group">
+            <input v-model="xProvider" class="form-control" placeholder="X-Provider ID">
+          </div>
+          <div class="description">
+            <b-alert
+              :show="showAlert"
+              @dismissed="showAlert = false"
+              variant="danger"
+              dismissible
+            >{{ errorMsg }}</b-alert>
 
-          <router-link class="description" to="/login">
-            <p>Zum Benutzer-Login</p>
-          </router-link>
+            <router-link class="description" to="/login">
+              <p>Zum Benutzer-Login</p>
+            </router-link>
+          </div>
+          <button type="button" v-on:click="sendPassword()" class="btn btn-primary">Anmelden</button>
         </div>
-        <button type="button" v-on:click="sendPassword()" class="btn btn-primary">Anmelden</button>
       </div>
     </div>
   </div>
