d34251e9552a75f8fc23c24af9b093f7563b2669
a_arzv01 <a_arzv01@uni-muenster.de>
2018-12-04 01:45:45 +0100

    Breadcrumbs not displayed any more

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -23,7 +23,8 @@
       </b-collapse>
     </b-navbar>
     <!--<div class="clearfisx"></div>-->
-    <breadcrumb-component></breadcrumb-component>
+    <!--<breadcrumb-component></breadcrumb-component>-->
+    <!--<footer-component></footer-component>-->
     <router-view></router-view>
   </div>
 </template>
