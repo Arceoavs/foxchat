c0f379df02f1085d650c74f327069b854148f050
Nils Neuhaus <nilsneuhaus@WL-POOL1-PNT-04846.UNI-MUENSTER.DE>
2018-12-10 19:59:04 +0100

    KP

--- resources/js/components/chat/ChatOverview.vue
+++ resources/js/components/chat/ChatOverview.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container>
-    <b-row class="mt-3">
+    <b-row class="mt-3" v-model="providers">
       <b-col>
         <chat-overview-component
           v-for="provideritem in providers"
@@ -17,25 +17,28 @@
 import ChatOverviewComponent from "./ChatOverviewComponent.vue";
 //import providerList from "./providerList.js";
 
-import {returnProviderList} from "../../services/ChatOverviewService.js";
+import { returnProviderList } from "../../services/ChatOverviewService.js";
 
 export default {
   data() {
     return {
-      providers: {test:"hallo"}
+      providers: { test: "hallo" }
     };
   },
-  mounted(){
+  mounted() {
     this.getProviderList();
-    console.log(this.providers)
+    console.log(this.providers);
+  },
+  watch: {
+    providers:function(){
+      this.providers = returnProviderList();
+    }
   },
   components: {
     ChatOverviewComponent
   },
   methods: {
-    getProviderList() {
-      returnProviderList().then(provider => this.providers = provider)
-    }
+    getProviderList() {}
   }
 };
 </script>
