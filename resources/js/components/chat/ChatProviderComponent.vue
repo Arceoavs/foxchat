b6bf189e39ee6f545fe14050efc918e7e763448b
Luis <l_meie13@uni-muenster.de>
2018-12-13 15:57:40 +0100

    added chat provider overview

---
+++ resources/js/components/chat/ChatProviderComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <b-jumbotron bg-variant="secondary" class="chatGroup mt-3">
+      <!-- Document Name -->
+      <b-card>
+        <div slot="header" style="text-align:left;">
+          {{userName}}
+          <span style="float:right;">{{timeStamp}}</span>
+        </div>
+        <!--Todo: Provider Communication -->
+        <router-link to="/communication">
+          <h4 class="left textColor">{{documentName}}</h4>
+          <font-awesome-icon class="cardIcon textFox" icon="comments" size="2x"/>
+        </router-link>
+      </b-card>
+    </b-jumbotron>
+  </div>
+</template>
+
+
+<script>
+import ChatListComponent from "./ChatListComponent.vue";
+
+export default {
+  props: ["documentName", "timeStamp", "userName"], //documentChats
+  data() {
+    return {
+      generalChat: "Allgemeiner Chat", //remove?
+      addChat: "Chat hinzufügen" //remove?
+    };
+  },
+  components: {
+    ChatListComponent
+  }
+};
+</script>
+
+<style>
+.chatGroup {
+  padding: 0.3em;
+  margin: 0.3em;
+  background-color: rgba(90, 83, 83, 0.1) !important;
+}
+.rotate-down {
+  -moz-transform: rotate(90deg);
+  -ms-transform: rotate(90deg);
+  -webkit-transform: rotate(90deg);
+  transform: rotate(90deg);
+}
+</style>
---
+++ resources/js/components/chat/ChatProviderOverview.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-container>
+    <b-row class="mt-3">
+      <b-col>
+        <!-- Chats -->
+        <chat-provider-component
+          v-for="chatItem in chats"
+          v-bind:key="chatItem.id"
+          v-bind:documentName="chatItem.document"
+          v-bind:timeStamp="chatItem.date"
+          v-bind:userName="chatItem.userName"
+        ></chat-provider-component>
+        <!-- Add chat -->
+        <b-jumbotron bg-variant="secondary" class="chatGroup mt-3">
+          <b-card>
+            <h4 class="left textColor">{{addChat}}</h4>
+            <div class="cardIcon textFox">
+              <font-awesome-icon icon="plus-circle" size="2x"/>
+            </div>
+          </b-card>
+        </b-jumbotron>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import ChatProviderComponent from "./ChatProviderComponent.vue";
+export default {
+  data() {
+    //TODO: Sortieren nach Datum (Idee: return ans Ende, vorher dann sortieren)
+    return {
+      chats: [
+        {
+          id: "1",
+          document: 'Chat zu "Dok. 123"',
+          date: "30.11.2018",
+          userName: "Dietrich"
+        },
+        {
+          id: "2",
+          document: 'Chat zu "Dok. 456"',
+          date: "06.12.2018",
+          userName: "Jacqueline"
+        },
+        {
+          id: "3",
+          document: 'Chat zu "Dok. 789"',
+          date: "11.12.2018",
+          userName: "Max Mustermann"
+        }
+      ],
+      addChat: "Neuen Chat starten"
+    };
+  },
+  components: {
+    ChatProviderComponent
+  }
+};
+</script>
+
+<style>
+.cardIcon {
+  float: right;
+}
+.left {
+  float: left;
+}
+</style>
