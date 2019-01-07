82c5fd06e519458402e0dd0fa1d304a0d4807767
BuildTools <unconfigured@null.spigotmc.org>
2019-01-07 17:22:32 +0100

    full commit, functional

---
+++ resources/js/components/documents/DocumentAggr.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <b-breadcrumb :items="items"/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      items: [
+        {
+          text: "Dokumente",
+          to: {name: 'Dokumente'}
+        },
+        {
+          text: "Provider",
+          to: {name: 'myproviders'}
+        },
+        {
+          text: "Child",
+          to: {name: 'children'}
+        }
+      ]
+    }
+  },
+
+  mounted() {
+    console.log("Nested Component mounted.");
+  }
+};
+</script>
