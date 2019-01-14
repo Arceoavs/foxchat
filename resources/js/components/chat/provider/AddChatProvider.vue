9f63045d6a31b8ebc1a1649d9cc6e406b88ae0e2
mbarkau <m_bark02@uni-muenster.de>
2019-01-14 16:17:50 +0100

    Added Router Link to new Chat with subscriber

--- resources/js/components/chat/provider/AddChatProvider.vue
+++ resources/js/components/chat/provider/AddChatProvider.vue
@@ -1,12 +1,16 @@
 <template>
     <b-jumbotron bg-variant="secondary" class="chatGroup mt-3">   
         <b-card>
-        <b-row>
-            <b-col cols="1" sm="2" md="1" class="textFox chatIcon">
-            <font-awesome-icon icon="plus-circle" size="2x"/>
-            </b-col>
-            <b-col><h4 class="textColor" v-text = "$ml.get('add_chat') + ' ' +this.username" /></b-col>
-        </b-row>
+        <router-link
+          :to="'/chat/communication?partner='+this.username+'&tag=allgemein'"
+          class="textColor chat-overview-link">
+            <b-row>
+                <b-col cols="1" sm="2" md="1" class="textFox chatIcon">
+                <font-awesome-icon icon="plus-circle" size="2x"/>
+                </b-col>
+                <b-col><h4 class="textColor" v-text = "$ml.get('add_chat') + ' ' +this.username" /></b-col>
+            </b-row>
+        </router-link>
         </b-card>
     </b-jumbotron>
 </template>
