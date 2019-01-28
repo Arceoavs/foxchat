bbf7139d82e3e56e9472c9ca59c8677cc36e60ff
mbarkau <m_bark02@uni-muenster.de>
2019-01-28 13:49:51 +0100

    documentation

--- resources/js/services/FoxdoxSubscriberService.js
+++ resources/js/services/FoxdoxSubscriberService.js
@@ -2,18 +2,30 @@ import { store } from '../store.js';
 
 import axios from 'axios';
 
+/**
+ * Service for provider, to list all its subscribers for starting general chat. 
+ */
 class FoxdoxSubscriberService {
+
+  /**
+   * method for accessing backend api and listing all subscribers, which
+   * are assigned to the logged in provider and haven't started a general
+   * chat yet.
+   */
     getSubscriberList() {
+        //setting bearer token to identify provider
         var config = {
           headers: {
             'Accept': 'application/json',
             Authorization: 'Bearer ' + localStorage.getItem('bearer')
           }
         };
+        //provider api path
         var path = '/api/foxdoxapi/provider';
     
         console.log('Getting Subscriber List');
     
+        //actual request with config previously setted.
         axios.get(path + '/listSubscribersWithoutGeneralChat', config)
           .then(response => {
             console.log('Listing Subscribers...');
