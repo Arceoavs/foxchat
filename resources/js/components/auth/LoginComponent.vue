1466104b088bf04d919b6e073c70d397ad7539c7
mbarkau <m_bark02@uni-muenster.de>
2019-01-29 19:51:59 +0100

    component doc

--- resources/js/components/auth/LoginComponent.vue
+++ resources/js/components/auth/LoginComponent.vue
@@ -3,6 +3,7 @@
     <b-row class="justify-content-center mt-5">
       <b-col md="4" sm="6" cols="8">
         <div class="panel">
+          <!--getting multilanguage strings, value will be determined by reading language property in ml module-->
           <h2 v-text="$ml.get('anmeldung')" />
           <p v-text="$ml.get('login_text')" />
         </div>
@@ -12,12 +13,15 @@
       <b-col md="4" sm="6" cols="8">
         <div @keyup.enter="sendPassword()">
           <div class="form-group">
+            <!--input field for username-->
             <input v-model="username" class="form-control" v-bind:placeholder="$ml.get('fox_username')">
           </div>
           <div class="form-group">
+            <!--input field for password-->
             <input v-model="password" type="password" class="form-control" v-bind:placeholder="$ml.get('password')">
           </div>
           <div class="description">
+            <!--displaying error messages if property is set-->
             <b-alert
               :show="showAlert"
               @dismissed="showAlert = false"
@@ -28,6 +32,7 @@
               <p v-text="$ml.get('provider_login')" />
             </router-link>
           </div>
+          <!--calling login method see javascript section for more documentation-->
           <button type="button" v-on:click="sendPassword()" class="btn btn-primary"><p v-text="$ml.get('login_button')"/></button>
         </div>
       </b-col>
@@ -40,6 +45,7 @@
 import auth from "../../services/AuthService.js";
 
 export default {
+  //defining properties of component
   data: function() {
     return {
       username: "",
@@ -50,10 +56,12 @@ export default {
     };
   },
   methods: {
+    //calling login method from AuthService for regular Users
     sendPassword: function() {
       auth.login(this.username, this.password, this);
     }
   },
+  //logging
   mounted() {
     console.log("Login Component mounted.");
   }
