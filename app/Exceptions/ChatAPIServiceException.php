6773c8b0f2baa47d3e95336d71b1174a8256de31
Nils Neuhaus <nilsneuhaus@Nilss-iMac.local>
2019-01-30 18:29:27 +0100

    Added comments to exceptions

--- app/Exceptions/ChatAPIServiceException.php
+++ app/Exceptions/ChatAPIServiceException.php
@@ -7,11 +7,18 @@ use Exception;
 class ChatAPIServiceException extends Exception
 {
     protected $chatobject;
+    /**
+     * This Exception will be used to define errors in the Chat Api.
+     */
     public function __construct($chatobject)
     {
         $this->chatobject= $chatobject;
     }
 
+    /**
+     * This Exception will be called when an error occured.
+     * @return JSON
+     */
     public function render()
     {
         return response()->json(
--- app/Exceptions/ChatAuthException.php
+++ app/Exceptions/ChatAuthException.php
@@ -7,14 +7,21 @@ use Exception;
 class ChatAuthException extends Exception
 {
     protected $chatobject;
+    /**
+     * This Exception will be used to determine if a Foxdox User called the Foxdox Provider Chat API
+     * and other way around.
+     */
     public function __construct($chatobject)
     {
-        $this->chatobject= $chatobject;
+        $this->chatobject = $chatobject;
     }
 
+    /**
+     * This Exception will be called when an error occured.
+     * @return JSON
+     */
     public function render()
     {
-        
         return response()->json(
             [
                 'errors' => [
