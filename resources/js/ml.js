724883c5c971174bf47aa8f0044d0d9af376e813
Eric Matala de Mazza <eric.matala@gmx.de>
2019-02-01 00:33:59 +0100

    home page is now sexy AF

--- resources/js/ml.js
+++ resources/js/ml.js
@@ -56,7 +56,7 @@ export default new MLCreate({
       this_is_the_general_chat: 'Allgemeiner Chat',
       open: 'Öffnen',
       new_message_toast: 'Neue Nachricht von',
-      homepage_sentence: "Willkommen bei FoxChat!",
+      homepage_sentence: "Willkommen bei",
       homepage_introduction: "Bitte wählen Sie Dokumente oder Chat aus, um zu beginnen.",
       hour_stamp: " Uhr",
       chat_input: "Schreiben Sie eine Antwort"
@@ -107,7 +107,7 @@ export default new MLCreate({
       this_is_the_general_chat: 'General chat',
       open: 'Open',
       new_message_toast: 'New message received from',
-      homepage_sentence: "Welcome to FoxChat!",
+      homepage_sentence: "Welcome to",
       homepage_introduction: "Please choose either Documents or Chat to get started.",
       hour_stamp: " o'clock",
       chat_input: "Write a reply"
--- resources/js/views/Homepage.vue
+++ resources/js/views/Homepage.vue
@@ -1,7 +1,30 @@
 <template>
-  <!-- The landing page to be displayed after successful login under the /homepage route-->
-  <div>
-    <h1 v-text="$ml.get('homepage_sentence')"></h1>
-    <h3 v-text="$ml.get('homepage_introduction')"></h3>
+  <div class="home">
+    <h1 class="heading" v-text="$ml.get('homepage_sentence')"></h1>
+    <img class="welcome" src="/img/FoxdoxChat.png">
+    <h3 class="bottom" v-text="$ml.get('homepage_introduction')"></h3>
   </div>
 </template>
+
+<script>
+</script>
+
+<style>
+.home {
+  text-align: center;
+  margin: auto;
+  padding-top: 10%;
+  color: rgb(86, 86, 86);
+  /* padding-bottom: 50%; */
+}
+.bottom {
+  padding-top: 1%;
+}
+.welcome {
+  width: 50%;
+  height: 50%;
+}
+.heading {
+  font-size: 350%;
+}
+</style>
